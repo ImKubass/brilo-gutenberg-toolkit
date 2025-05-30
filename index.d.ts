4e48f5ad81f5069bef453cc1aeabe380ae982661
Jakub <jakubjetleb@gmail.com>
2025-05-30 16:53:00 +0200

    Add index.d.ts for type definitions and bump version to 0.1.2

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+import type ButtonControl from "./src/components/ButtonControl/ButtonControl"
+import type ButtonGroupControl from "./src/components/ButtonGroupControl/ButtonGroupControl"
+import type ContentWrapper from "./src/components/ContentControl/ContentControl"
+import type HeaderControl from "./src/components/HeaderControl/HeaderControl"
+import type {HEADING_STYLES, HEADING_LEVELS, VARIANTS} from "./src/components/HeaderControl/types"
+import type HeadingLevelControl from "./src/components/HeadingLevelControl/HeadingLevelControl"
+import type MediaControl from "./src/components/MediaControl/MediaControl"
+import type MediaControlEdit from "./src/components/MediaControl/MediaControlEdit"
+import type PriceControl from "./src/components/PriceControl/PriceControl"
+import type PriceControlEdit from "./src/components/PriceControl/PriceControlEdit"
+import type SelectPostsControl from "./src/components/SelectPostsControl/SelectPostsControl"
+import type getMediaHandlers from "./src/hooks/getMediaHandlers"
+import type getBlockContentSerialized from "./src/hooks/getBlockContentSerialized"
+import type getChildBlockAttributes from "./src/hooks/getChildBlockAttributes"
+
+export {
+  getMediaHandlers,
+  getBlockContentSerialized,
+  getChildBlockAttributes,
+  ButtonControl,
+  ButtonGroupControl,
+  ContentWrapper,
+  HeaderControl,
+  HEADING_STYLES,
+  HEADING_LEVELS,
+  VARIANTS,
+  HeadingLevelControl,
+  MediaControl,
+  MediaControlEdit,
+  PriceControl,
+  PriceControlEdit,
+  SelectPostsControl,
+}
