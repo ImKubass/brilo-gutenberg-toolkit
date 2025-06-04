aa44d1033058d54f11385fb69121dd0dcbd9b819
Jakub <jakubjetleb@gmail.com>
2025-06-04 12:21:12 +0200

    Fix padding syntax and ensure consistent styling in MediaControlEdit.scss

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '32a778d509a00de57292');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b16ea907136ec5592e6f');
