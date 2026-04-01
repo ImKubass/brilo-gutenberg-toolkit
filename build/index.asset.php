c76c003e47ec1295de17f4165f4ab8bf7a89ec86
JakubJetleb <jakubjetleb@gmail.com>
2026-04-01 09:31:47 +0200

    fix: improve fetchPosts handling and add drag-and-drop functionality

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '64ca65cfc3b485a5650b');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '89bfff903e7a68ec5a32');
